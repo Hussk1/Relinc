4285944448f01bcb3479b45645199be565f22ca6
Hussk1 <husseinalhejami72@gmail.com>
2026-06-08 00:25:45 +0200

    Testing

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/hem/",
+  base: "/relinc/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
